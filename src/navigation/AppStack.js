3971e0785f7f169eae7e81b8f6a610153ba5f3e9
Bamlak <54115150+bamlak-kal@users.noreply.github.com>
2022-09-08 02:34:34 +0100

    Navigation stack fixed

---
+++ src/navigation/AppStack.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppProvider } from '@app/contexts/AppContext';
+import { GameScreen, LoadingScreen, HomeScreen } from '@app/screens';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+const NavigationStack = () => {
+  return (
+    <NavigationContainer>
+      <AppProvider>
+        <Navigator screenOptions={{ headerShown: false }}>
+          <Screen name="home" component={HomeScreen} />
+
+          <Screen name="game" component={GameScreen} />
+
+          <Screen name="loading" component={LoadingScreen} />
+        </Navigator>
+      </AppProvider>
+    </NavigationContainer>
+  );
+};
+
+export default NavigationStack;
---
+++ src/screens/index.js
@@ -0,0 +1,4 @@
+import LoadingScreen from './LoadingScreen';
+import GameScreen from './GameScreen';
+import HomeScreen from './HomeScreen';
+export { LoadingScreen, GameScreen, HomeScreen };
