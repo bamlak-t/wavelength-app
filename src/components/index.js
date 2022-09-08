d53d6b0a935c3e02d85d66ec53431a52aab9cd96
Bamlak <54115150+bamlak-kal@users.noreply.github.com>
2022-09-08 02:33:05 +0100

    Created header component

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Spinner from './Spinner/SpinnerComponent';
+import MainHeader from './MainHeader';
+import PromptComponent from './PromptComponent';
+
+export { Spinner, MainHeader, PromptComponent };
