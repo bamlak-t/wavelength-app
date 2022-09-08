5805046d013664f7fc240b5d7bf91d4a01bd6a78
Bamlak <54115150+bamlak-kal@users.noreply.github.com>
2022-09-09 00:00:54 +0100

    Fixed CoverComponent

---
+++ src/components/Spinner/CoverComponent.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, Animated, Dimensions, ImageBackground } from 'react-native';
+import { withAnchorPoint } from 'react-native-anchor-point';
+
+const { width } = Dimensions.get('window');
+const customWidth = width * 0.95;
+const customHeight = customWidth / 2;
+
+const Component = ({ props, children }) => {
+  const { rotateValue } = props;
+
+  const getTransform = () => {
+    let transform = {
+      transform: [{ perspective: 400 }, { rotateZ: rotateValue }],
+    };
+    return withAnchorPoint(transform, { x: 0.5, y: 1 }, { customWidth, customHeight });
+  };
+
+  return (
+    <Animated.View style={[styles.cover, getTransform()]}>
+      <ImageBackground
+        source={require('@app/assets/images/main-bg1.jpg')}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        {children}
+      </ImageBackground>
+    </Animated.View>
+  );
+};
+
+export default Component;
+
+const styles = StyleSheet.create({
+  cover: {
+    // flex: 1,
+    width: customWidth,
+    height: customWidth,
+    alignSelf: 'center',
+    borderRadius: customHeight,
+    overflow: 'hidden',
+  },
+  image: {
+    height: customHeight,
+    width: customWidth,
+    // justifyContent: 'center',
+  },
+});
