f587f56813c7e4b1685f0690391416ac211277fb
Bamlak <54115150+bamlak-kal@users.noreply.github.com>
2022-09-09 00:05:18 +0100

    Missed logic error

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Spinner from './Spinner/SpinnerComponent';
-import PromptComponent from './PromptComponent';
 import CoverComponent from './Spinner/CoverComponent';
+import PromptComponent from './PromptComponent';
 import ProgressComponent from './ProgressComponent';
 import GameOverComponent from './GameOverComponent';
 export { Spinner, PromptComponent, CoverComponent, ProgressComponent, GameOverComponent };
